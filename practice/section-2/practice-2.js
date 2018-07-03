0bc4cc8658bf02a14ad6af1d867f308cb25c72ee
YAOCH2 <christine.s.yao@oocl.com>
2018-07-03 22:42:26 +0800

    practice2-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,31 @@
 'use strict';
 
+'use strict';
+
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result=[];
+  var object={};
+  var object2={};
+  for(var i=0;i<collection.length;i++){
+    if(object.hasOwnProperty(collection[i])){
+      object[collection[i]]++;
+    }else{
+      if(collection[i].length>1){
+        if(object.hasOwnProperty(collection[i][0])){
+          object[collection[i][0]] += parseInt(collection[i][2]);
+        }else{
+          object[collection[i][0]]=parseInt(collection[i][2]);
+        }
+      }else{
+        object[collection[i]]=1;
+      }
+    }
+  }
+  for(var j in object){
+    object2.key=j;
+    object2.count=object[j];
+    result.push(object2);
+    object2={};
+  }
+  return result;
 }
